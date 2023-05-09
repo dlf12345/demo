a1e8bb1bdc5a9478da573619218df4e874d8acd3
dulongfei <2237570058@qq.com>
2023-05-09 22:08:18 +0800

    123

--- src/main/java/com/example/demo/Test/demo01.java
+++ src/main/java/com/example/demo/Test/demo01.java
@@ -2,10 +2,6 @@ package com.example.demo.Test;
 
 public class demo01 {
     public static void main(String[] args) {
-        System.out.println("1342424");
-        System.out.println("1234567");
-        System.out.println("hotfix");
-        System.out.println("hotfix2");
-        System.out.println("aadfsfs");
+        System.out.println("1211111111222");
     }
 }
